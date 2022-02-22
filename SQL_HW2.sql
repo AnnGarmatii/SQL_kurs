db7c987f2675ad05611124463ab4b906d04d1dcb
Anna <97239704+AnnGarmatii@users.noreply.github.com>
2022-02-22 23:00:00 +0300

    Update SQL_HW2.sql

--- SQL_HW2.sql
+++ SQL_HW2.sql
@@ -1,4 +1,4 @@
---Создать таблицу employees
+--РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employees
 --- id. serial,  primary key,
 --- employee_name. Varchar(50), not null
 
@@ -7,7 +7,7 @@ id serial primary key,
 employee_name varchar (50) not null
 );
 
---Наполнить таблицу employees 70 строками.
+--РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employees 70 СЃС‚СЂРѕРєР°РјРё.
 
 insert into employees(employee_name)
 values ('Peter Black'),
@@ -83,7 +83,7 @@ values ('Peter Black'),
 
 
 
---Создать таблицу salary
+--РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ salary
 --- id. Serial  primary key,
 --- monthly_salary. Int, not null
 
@@ -92,7 +92,7 @@ id serial primary key,
 monthly_salary int not null
 );
 
---Наполнить таблицу salary 15 строками:
+--РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ salary 15 СЃС‚СЂРѕРєР°РјРё
 
 insert into salary(monthly_salary)
 values(1000),
@@ -112,7 +112,7 @@ values(1000),
 	(2400),
 	(2500);
 
---Создать таблицу employee_salary
+--РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary
 --- id. Serial  primary key,
 --- employee_id. Int, not null, unique
 --- salary_id. Int, not null
@@ -124,8 +124,8 @@ employee_id int not null unique,
 salary_id int not null
 );
 
---Наполнить таблицу employee_salary 40 строками:
---- в 10 строк из 40 вставить несуществующие employee_id
+--РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary 40 СЃС‚СЂРѕРєР°РјРё
+--- РІ 10 СЃС‚СЂРѕРє РёР· 40 РІСЃС‚Р°РІРёС‚СЊ РЅРµСЃСѓС‰РµСЃС‚РІСѓСЋС‰РёРµ employee_id
 insert into employee_salary(employee_id, salary_id)
 values	(3,7),
 (1,4),
@@ -169,7 +169,7 @@ values	(3,7),
 (654,4);
 
 
---Создать таблицу roles
+--РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles
 --- id. Serial  primary key,
 --- role_name. int, not null, unique
 
@@ -178,11 +178,11 @@ id serial primary key,
 role_name int not null unique
 );
 
---Поменять тип столбца role_name с int на varchar(30)
+--РџРѕРјРµРЅСЏС‚СЊ С‚РёРї СЃС‚РѕР»Р±Р° role_name СЃ int РЅР° varchar(30)
 alter table roles
 alter column role_name type varchar(30);
 
---Наполнить таблицу roles 20 строками:
+--РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles 20 СЃС‚СЂРѕРєР°РјРё
 insert into roles(role_name)
 values ('Junior Python developer'),
 	('Middle Python developer'),
@@ -207,10 +207,10 @@ values ('Junior Python developer'),
 	
 
 	
---Создать таблицу roles_employee
+--РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee
 --- id. Serial  primary key,
---- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
---- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+--- employee_id. Int, not null, unique (ГўГ­ГҐГёГ­ГЁГ© ГЄГ«ГѕГ· Г¤Г«Гї ГІГ ГЎГ«ГЁГ¶Г» employees, ГЇГ®Г«ГҐ id)
+--- role_id. Int, not null (ГўГ­ГҐГёГ­ГЁГ© ГЄГ«ГѕГ· Г¤Г«Гї ГІГ ГЎГ«ГЁГ¶Г» roles, ГЇГ®Г«ГҐ id)
 
 create table roles_employee( 
 id serial  primary key,
@@ -222,7 +222,7 @@ foreign key (role_id)
 	references roles (id)
 );
 
---Наполнить таблицу roles_employee 40 строками
+--РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee 40 СЃС‚СЂРѕРєР°РјРё
 insert into roles_employee(employee_id, role_id)
 values (7,2),
 	(20,4),
